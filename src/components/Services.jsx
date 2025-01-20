9843c9f0df1886d040e3239ae8dbc419d81d4836
Piver <priverdeprince@yahoo.com>
2025-01-20 12:48:35 +0300

    Working on services Branc

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,9 +3,15 @@
 function Services() {
   return (
     <div>
-        <h1>This is Services Component</h1>
+      <h1>This is Services Component</h1>
+      <b>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis
+        culpa molestias officia sint! Odio possimus quis dolor perspiciatis
+        voluptatum ratione earum nulla eius error totam deleniti, distinctio
+        debitis adipisci dolore.
+      </b>
     </div>
-  )
+  );
 }
 
-export default Services
+export default Services;

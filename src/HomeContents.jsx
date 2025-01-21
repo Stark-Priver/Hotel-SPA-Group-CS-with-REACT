a27bb857f6c8a155c02120f9721d22d6e00443d0
Piver <priverdeprince@yahoo.com>
2025-01-21 15:13:25 +0300

    Added the Contact us on the Footer

--- src/HomeContents.jsx
+++ src/HomeContents.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 import Navigation from './components/Navigation'
 import Testimonials from './components/Testimonials'
 import Services from './components/Services'
+import Footer from './components/Footer'
 
 function HomeContents() {
   return (
     <div>
       <Navigation />
-      <Testimonials /> 
-      <Services /> 
+      <Testimonials />
+      <Services />
+      <Footer />  
     </div>
-  )
+  );
 }
 
 export default HomeContents
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,17 @@
 function Footer() {
   return (
     <div>
+      <div>
         <h2>This is the footer Component</h2>
+      </div>
+      <div>
+        <h2>CONTACT US</h2>
+        <p>Phone: 0713355737 </p>
+        <p>Email: info@spa.com </p>
+        <p>Address: Inyala Street, Mbeya, Tanzania</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
